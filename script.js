92944fcb6034af19994441e7c8d1d71b44045717
feather-art-ist <feather.art.ist.21@gmail.com>
2021-11-02 00:56:39 +0200

    added the methods, were planed for the settings

--- script.js
+++ script.js
@@ -25,3 +25,37 @@ const UserView = {
         window.scroll(0, this.getHightOfSettingModule())
     }
 }
+
+const Settings = {
+    inputs: {
+        wordInputValue(){
+            return document.forms[0].enteredEnglishWord.value
+        },
+        translationInputValue(){
+            return document.forms[0].enteredTranslation.value
+        },
+        resetWordInputValue(str = ''){
+            document.forms[0].enteredEnglishWord.value = str
+        },
+        resetTranslationInputValue(str = ''){
+            document.forms[0].enteredTranslation.value = str
+        },
+    },
+    buttons: {
+        addNewWord(newWord = false, newTranslation = false){
+        },
+        addNewTranslationForOldWord(word = false, newTranslation = false){
+        },
+        deleteWrittenWord(word = false, translation = false){
+        },
+        deleteWrittenTranslationForWord(word = false, translation = false){
+        }
+    }
+}
+
+const WordsStor = {
+    Includes(word = false, translation = false){
+    },
+    clear(){
+    }
+}
