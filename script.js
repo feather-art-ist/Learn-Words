a2abd800182245128656d7d4e0a62ee50380d044
feather-art-ist <feather.art.ist.21@gmail.com>
2021-11-03 23:46:01 +0200

    methods for Dictionary satting

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// VIEW
+
 const Theme = {
     now: 'light',
     changeTheme(){
@@ -6,7 +8,7 @@ const Theme = {
                 document.head.querySelector('link').href = 'styleDark.css'
                 document.querySelector('.themeIcon').src = 'img/sun.png'
                 this.now = 'dark'
-                break;
+            break;
         
             case 'dark':
                 document.head.querySelector('link').href = 'styleLight.css'
@@ -26,6 +28,122 @@ const UserView = {
     }
 }
 
+// GAME
+
+let Dictionary = {}
+//  Dictionary {
+//      0: {
+//          word: 'word user is learning',
+//          translation: ['array', 'with', 'translations', 'of', 'word']
+//      }
+//      1:  {...}
+//      ...
+//  }
+
+const DictionaryMethods = {
+    includes(word = '', translation = ''){
+
+        // this method returns Object with three keys, where:
+        //  - Object.DictionaryHasThisWord:
+        //      - true if Dictionary has this word
+        //      - false if Dictionary hasn't this word
+        //  - Object.indexOfWordInDictionary:
+        //      - key of this word in Dictionary if Dictionary has this word
+        //      - false if Dictionary hasn't this word
+        //  - Object.wordHasSuchTranslation:
+        //      - true if this word was found and one has this translation
+        //      - false if this word wasn't found or one hasn't such translation
+
+        let answer = {
+            DictionaryHasThisWord: false,
+            indexOfWordInDictionary: false,
+            wordHasSuchTranslation: false,
+        }
+
+        for(let key in Dictionary){
+            if( Dictionary[key].word === word ){
+                answer.DictionaryHasThisWord = true
+                answer.indexOfWordInDictionary = key
+
+                if( Dictionary[key].translation.includes(translation) ){
+                    answer.wordHasSuchTranslation = true
+                }
+            }
+        }
+
+        return answer
+    },
+
+    getTheFirstFreeKey(){
+        let i = 0
+
+        for(let key in Dictionary){
+            i++
+        }
+
+        return i
+    },
+
+    addNewWord(newWord, translation){
+        let newKey = this.getTheFirstFreeKey()
+
+        Dictionary[newKey] = {}
+        Dictionary[newKey].word = newWord
+        Dictionary[newKey].translation = [translation]
+
+        WordsStor.resetDictionaryInLocalStorage(Dictionary)
+    },
+
+    addNewTranslationForOldWord(OldWordIndex, newTranslation){
+        Dictionary[OldWordIndex].translation.push(newTranslation)
+
+        WordsStor.resetDictionaryInLocalStorage(Dictionary)
+    },
+
+    newWordOrNewTranslation(word, translation){
+        let checkResult = this.includes(word, translation)
+        if(checkResult.DictionaryHasThisWord){
+            if(checkResult.wordHasSuchTranslation){
+                console.log('Dictionary already has this word with such translation') // in the future, a user will see message whit this information
+            }else{
+                this.addNewTranslationForOldWord(checkResult.indexOfWordInDictionary, translation)
+            }
+        }else{
+            this.addNewWord(word, translation)
+        }
+    },
+
+    deleteWord(key){
+        delete Dictionary[key]
+
+        WordsStor.resetDictionaryInLocalStorage(Dictionary)
+    },
+
+    deleteTranslationOfWord(key, unnecessaryTranslation){
+        let indexOfUnnecessaryTranslation = Dictionary[key].translation.indexOf(unnecessaryTranslation)
+        Dictionary[key].translation.splice(indexOfUnnecessaryTranslation, 1)
+
+        WordsStor.resetDictionaryInLocalStorage(Dictionary)
+    },
+
+    deleteWordOrTranslation(word, translation){
+        let checkResult = this.includes(word, translation)
+        if(checkResult.DictionaryHasThisWord){
+            if(checkResult.wordHasSuchTranslation){
+                this.deleteTranslationOfWord(checkResult.indexOfWordInDictionary, translation)
+            }else if(translation === ''){
+                this.deleteWord(checkResult.indexOfWordInDictionary)
+            }else{
+                console.log('Word has not such translation') // in the future, a user will see message whit this information
+            }
+        }else{
+            console.log('Dictionary has not this word') // in the future, a user will see message whit this information
+        }
+    }
+}
+
+// SETTING
+
 const Settings = {
     inputs: {
         wordInputValue(){
@@ -42,20 +160,30 @@ const Settings = {
         },
     },
     buttons: {
-        addNewWord(newWord = false, newTranslation = false){
+        addThisButton(){
         },
-        addNewTranslationForOldWord(word = false, newTranslation = false){
-        },
-        deleteWrittenWord(word = false, translation = false){
-        },
-        deleteWrittenTranslationForWord(word = false, translation = false){
+        deleteThisButton(){
         }
     }
 }
 
+// LOCAL_STORAGE
+
 const WordsStor = {
-    Includes(word = false, translation = false){
+    getDictionaryFromLocalStorage(){
+        return JSON.parse(localStorage.getItem('dictionaryJSON'))
+    },
+    resetDictionaryInLocalStorage(dictionaryObject){
+        localStorage.setItem( 'dictionaryJSON', JSON.stringify(dictionaryObject))
     },
     clear(){
+        localStorage.clear() // this method there is for the future. User will be able to clear his localStore (his Dictionary)
     }
+}
+
+
+// START GAME
+
+if(localStorage.propertyIsEnumerable('dictionaryJSON')){
+    Dictionary = {...WordsStor.getDictionaryFromLocalStorage()}
 }
